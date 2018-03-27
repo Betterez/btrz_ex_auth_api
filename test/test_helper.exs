5d8294da1e8de8b59f76fe8ba57a15ec643faa09
pbrudnick <pbrudnick@gmail.com>
2018-03-27 11:29:50 -0300

    reverting configuration, using test.exs config

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,1 @@
-Application.put_env(:btrz_auth, :token, [issuer: "btrz-api-client",
-  main_secret: "NfUeAlyXW4ItToJokpFpdd4b5BhDGEE+3b9stOChnhkuLjRPMOgIVcRIqP8jCDeV",
-  secondary_secret: "t0PCMbBHkow/PUPH3Cz5gj66MrTI5t22iNCT2et3FqtHVvjhyDJ6kqOtrKTI6l3b",
-  test_resource: %{"id" => "gj66MrTI5t2"}])
-
 ExUnit.start()
