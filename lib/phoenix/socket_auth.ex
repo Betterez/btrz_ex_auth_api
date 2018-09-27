70ebfffa7114173e2b429d35b97391e440402c4d
Pablo Brudnick <pbrudnick@gmail.com>
2018-09-27 11:36:50 -0300

    ensure Phoenix loaded just for authenticate fn

--- lib/phoenix/socket_auth.ex
+++ lib/phoenix/socket_auth.ex
@@ -1,14 +1,16 @@
-if Code.ensure_loaded?(Phoenix) do
-  defmodule BtrzAuth.Phoenix.SocketAuth do
-    @moduledoc """
-    This module should be used only by Phoenix Sockets.
-    """
+defmodule BtrzAuth.Phoenix.SocketAuth do
+  @moduledoc """
+  This module should be used only by Phoenix Sockets.
+  """
 
-    @doc """
-    Wraps the `Guardian.Phoenix.Socket.authenticate` using the `BtrzAuth.Phoenix.Socket` implementation module.
-    """
-    def authenticate(socket, token) do
+  @doc """
+  Wraps the `Guardian.Phoenix.Socket.authenticate` using the `BtrzAuth.Phoenix.Socket` implementation module.
+  """
+  def authenticate(socket, token) do
+    if Code.ensure_loaded?(Phoenix) do
       Guardian.Phoenix.Socket.authenticate(socket, BtrzAuth.Phoenix.Socket, token)
+    else
+      :error
     end
   end
 end
